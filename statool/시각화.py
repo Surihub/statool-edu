49d35056c653d72bedd48e1c3402d532527823a5
surish <sbhath17@gmail.com>
2023-09-15 14:05:14 +0900

    인공지능 툴 추가

--- statool/시각화.py
+++ statool/시각화.py
@@ -31,3 +31,20 @@ def 원그래프(원자료, 열이름):
     plt.show()
 
 
+def 산점도(원자료, 가로열이름, 세로열이름, 구분=None):
+    plt.figure(figsize=(8, 6))
+    
+    if 구분 is None:
+        sns.scatterplot(data=원자료, x=가로열이름, y=세로열이름)
+        plt.title(f'{가로열이름} vs. {세로열이름} 산점도')
+    else:
+        sns.scatterplot(data=원자료, x=가로열이름, y=세로열이름, hue=구분, palette='Set1')
+        plt.title(f'{가로열이름} vs. {세로열이름} 산점도 (구분: {구분})')
+    
+    plt.xlabel(가로열이름)
+    plt.ylabel(세로열이름)
+    plt.legend(loc='best')
+    plt.show()
+
+# heatmap
+# 전체 플롯
